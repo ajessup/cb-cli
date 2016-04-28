acb6b58ae65ca681547cb017c444ca43fd9512e6
Andrew Jessup <jessup@jessup-macbookpro.roam.corp.google.com>
2016-04-28 08:50:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# cb-cli - An Unofficial CLI tool for Google Cloud Container Builder
+
+Google Cloud Container Builder is a powerful tool for building Container Images
+quickly and efficiently from source and storing them in Google Container
+Registry (GCR). While Container Builder supports Dockerfiles to configure builds
+it also supports other methods of building images more safely and efficiently.
+
+This CLI provides a handy mechanism for accessing the API for building Container
+images from local source, and introduces Buildfiles for describing Container
+Builder configurations.
+
+## Authentication
+
+This tool uses Application Default Credentials to authenticate to the Google
+Cloud Platform. To use these, download the Google Cloud SDK and run:
+
+ `gcloud beta auth application-default login`
+
+prior to running the script. Read more on Google's
+[documentation](https://developers.google.com/identity/protocols/application-default-credentials#whentouse)
+
+## Basic Usage
+
+To build a Container Image from source, add a Buildfile to the base directory
+of the application's source tree, then run the Container Builder CLI.
+
+ ./cb-cli.py gcr.io/your-project-id/image-name
+
+The Buildfile describes how your source code should be transformed into a Docker
+image, and is run remotely using the Cloud Container Builder service.
+
+## Buildfiles (not yet supported)
+
+Buildfiles describe a set of *build steps* that are run in serial against your
+source tree to produce a Docker image. 
+
+To help get you started, here are some useful Buildfiles.
+
+* Build any Docker container from a Dockerfile
+* Build a lean Spring Boot application from source using a Maven pom.xml file
+* Build a Node.js application from a Gulp configuration file
+
+A build step is itself simply a Docker image within which your
+source will be transiently mounted in a special directory. You can customize
+the arguments, environment variables and parameters used to invoke a build step,
+making them easy to configure.
---
+++ cb-cli.py
@@ -0,0 +1,158 @@
+import click
+import base64
+import hashlib
+from googleapiclient import discovery
+from googleapiclient import http
+from googleapiclient.errors import HttpError
+import json
+import tarfile
+import tempfile
+import time
+import os
+
+from oauth2client.client import GoogleCredentials
+
+@click.command()
+@click.argument('image')
+@click.argument('path', default='.', type=click.Path(exists=True))
+@click.option('--buildfile', default=None,
+              help='Path to the buildfile to use to configure the build')
+@click.option('--bucket', default=None,
+              help='Bucket to store source archive in. Defaults to \%projectname\%-cbstorage')
+@click.option('--project-id', default=None,
+              help='Cloud platform project ID. If not set, will look in the environment variable GCP_PROJECT_ID.')
+def build(path, image, buildfile, project_id, bucket):
+    path = os.path.join(os.getcwd(), path)
+
+    if buildfile is None:
+        buildfile = os.path.join(path, 'Buildfile')
+
+    project_id = project_id or os.getenv('GCP_PROJECT_ID')
+    if project_id is None:
+        raise click.UsageError('No project ID specified. Pass option --project-id or set GCP_PROJECT_ID')
+
+    if bucket is None:
+        bucket = '%s-cbstorage' % project_id
+
+    credentials = GoogleCredentials.get_application_default()
+
+    buildfile = os.path.abspath(buildfile)
+    if not os.path.exists(buildfile):
+        raise click.UsageError('Could not find buildfile %s, please add one or use --buildfile to specify one' % buildfile)
+
+    click.echo('Building %s to %s with %s' % (path, image, buildfile))
+
+    # Create temporary archive for the source directory
+    archive = tempfile.NamedTemporaryFile(delete=False,suffix='.tar.gz')
+    tar = tarfile.open(fileobj=archive, mode='w:gz')
+
+    click.echo("Archiving %s to %s" % (path, archive.name))
+    for spath, subdirs, files in os.walk(path):
+        for name in files:
+            print 'Adding %s' % os.path.relpath(os.path.join(spath, name),path)
+            tar.add(os.path.join(spath, name), recursive=False,
+                    arcname=os.path.relpath(os.path.join(spath, name),path))
+
+    tar.close()
+    archive.close()
+    # hash_md5 = hashlib.md5()
+    # with open(archive.name, "rb") as f:
+    #     for chunk in iter(lambda: f.read(4096), b""):
+    #         hash_md5.update(chunk)
+
+    # Upload the source directory to GCS
+    click.echo('Checking for bucket %s...' % bucket)
+    gcs_service = discovery.build('storage', 'v1', credentials=credentials)
+    req = gcs_service.buckets().get(bucket=bucket)
+    try:
+        req.execute()
+    except HttpError, hell:
+        if hell.resp.status == 404:
+            click.echo('Bucket %s not found, attempting to create it...' % bucket)
+            body = {
+                'name': bucket,
+            }
+            req = gcs_service.buckets().insert(project=project_id, body=body)
+            resp = req.execute()
+        else:
+            raise hell
+
+    click.echo('Uploading %s to %s...' % (os.path.basename(archive.name), bucket))
+    try:
+        body = {
+            'name': os.path.basename(archive.name),
+        }
+        media = http.MediaFileUpload(archive.name, mimetype='application/x-gzip', chunksize=4194304, resumable=True)
+        req = gcs_service.objects().insert(
+            bucket=bucket,
+            name=os.path.basename(archive.name),
+            media_body=media,
+            body={"cacheControl": "public,max-age=31536000"}
+        )
+        resp = None
+        while resp is None:
+            status, resp = req.next_chunk()
+            if status:
+                print "Uploaded %d%%." % int(status.progress() * 100)
+        click.echo('...done!')
+    except HttpError, amy:
+        if amy.resp.status == 403:
+            raise click.UsageError('You don\'t have permission to write to GCS bucket %s. Fix this or specify a different bucket to use.' % bucket)
+        else:
+            raise amy
+
+    # Invoke the container builder API
+    ccb_service = discovery.build('cloudbuild', 'v1', credentials=credentials,
+        discoveryServiceUrl="https://content-cloudbuild.googleapis.com/$discovery/rest?version=v1")
+
+    body = {
+        "source": {
+            "storageSource": {
+                "bucket": bucket,
+                "object": os.path.basename(archive.name)
+            }
+        },
+        "steps": [
+            {
+                "name": "gcr.io/cloud-builders/dockerizer",
+                "args": [
+                    image
+                ]
+            }
+        ],
+        "images": [
+            image
+        ]
+    }
+
+    # req = ccb_service.projects().builds().list(projectId=project_id)
+    # resp = req.execute()
+    # print(json.dumps(resp, indent=2))
+
+    req = ccb_service.projects().builds().create(
+        projectId=project_id, body=body)
+
+    resp = req.execute()
+
+    if resp['metadata']['build']['status'] == 'QUEUED':
+        click.echo('Queued build %s' % resp['metadata']['build']['id'])
+
+        operation_id = resp['name']
+        while (resp['metadata']['build']['status'] == 'QUEUED' or
+              resp['metadata']['build']['status'] == 'WORKING'):
+            resp = ccb_service.operations().get(name=operation_id).execute()
+            click.echo('Building... %s' % resp['metadata']['build']['status'])
+            time.sleep(1)
+
+    if resp['metadata']['build']['status'] == 'SUCCESS':
+        resp = ccb_service.operations().get(name=operation_id).execute()
+        for image in resp['metadata']['build']['results']['images']:
+            click.echo('Built %s' % image['name'])
+            click.echo('(Image digest: %s)' % image['digest'])
+    else:
+        click.echo('Build returned %s - check build ID %s' %
+                   (resp['metadata']['build']['status'],
+                    resp['metadata']['build']['id']))
+
+if __name__ == '__main__':
+    build()
